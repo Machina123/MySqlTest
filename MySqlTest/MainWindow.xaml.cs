34c63560342a9c27bb4f30d26baa2cb381f78fa5
Machina <machina123@boun.cr>
2015-06-10 22:09:31 +0200

    Initial commit

---
+++ MySqlTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MySqlTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MySqlConnectionStringBuilder connString = new MySqlConnectionStringBuilder();
+                connString.Server = tbAddress.Text;
+                connString.UserID = tbUsername.Text;
+                connString.Password = tbPassword.Password;
+                connString.Database = tbDatabase.Text;
+
+                MySqlConnection conn = new MySqlConnection(connString.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message.ToString());
+            }
+        }
+
+    }
+}
